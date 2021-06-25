472506fc05fd6b8ec731315eb5d6365f9150c215
gajen <meetgajen@yahoo.com>
2021-06-25 10:06:24 -0400

    commit tree 8e171518c679475083ec7e13c24be179ea755368

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,34 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+constructor(){
+    super()
+    this.state ={
+        timesClicked: 0
+    }
+}
+
+handleClicked = ()=>{
+    console.log('hello')
+    const increase = this.state.timesClicked + 1
+    this.setState({
+        timesClicked: increase
+    })
+
+}
+
+
+
+    
+    render() {
+        return (
+            <div>
+                <button onClick = {this.handleClicked}>{this.state.timesClicked}</button>
+                {this.timesClicked}
+            </div>
+        );
+    }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,59 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDebugger extends Component {
+constructor(props){
+    super()
+    this.state ={
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+    this.handleClickBitrate = this.handleClickBitrate.bind(this);
+    this.handleClickSimulate = this.handleClickSimulate.bind(this)
+
+}
+
+handleClickSimulate(){
+    console.log('hello')
+    this.setState({
+        settings: Object.assign({}, this.state.settings, {
+            video: Object.assign({}, this.state.settings.video,{
+                resolution: '720p'
+            })
+        })
+    })
+
+}
+
+
+
+handleClickBitrate (event) {
+    console.log('hello')
+    this.setState({
+      settings: Object.assign({}, this.state.settings, {
+        bitrate: 12
+      }),
+    });
+  }
+
+
+
+
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleClickBitrate}>Change bitrate</button>
+                <button className= "resolution" onClick={this.handleClickSimulate}>Change Resolution</button>
+            </div>
+        );
+    }
+}
+
+export default YouTubeDebugger;
